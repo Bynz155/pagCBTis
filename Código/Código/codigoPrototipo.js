ba93f5bfbd0e3104d8313ed430ae23eaa85890e6
Bynz155 <bghv155@gmail.com>
2023-05-30 14:54:29 -0600

    elimine archivos extra

--- Código/Código/codigoPrototipo.js
+++
@@ -1,6 +0,0 @@
-const buttonCategorias = document.querySelector("#button2");
-const navCategorias = document.querySelector(".container__menu__categorias")
-
-buttonCategorias.addEventListener("click", () => {
-    navCategorias.classList.toggle("activo");
-})
--- Código/Código/codigoSubMenuEcologia.js
+++
@@ -1,6 +0,0 @@
-const buttonSubMenu = document.querySelector(".button__submenu__ecologia");
-const ulSubMenu = document.querySelector(".ul__submenu__ecologia")
-
-buttonSubMenu.addEventListener("click", () => {
-    ulSubMenu.classList.toggle("activo");
-})
